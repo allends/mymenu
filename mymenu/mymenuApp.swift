4eeb922aaa49296a1d5ce4696407ed6df6feefad
allends <allen.davisswing@rutgers.edu>
2023-06-01 18:27:38 -0400

    Initial Commit

---
+++ mymenu/mymenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  mymenuApp.swift
+//  mymenu
+//
+//  Created by Allen Davis-Swing on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct mymenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
